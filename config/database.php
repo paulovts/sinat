1a37fc48fabccb2cae908836df7a84b34a5589ae
Paulo Victor Teles de Sousa <paulovictor.teles@gmail.com>
2020-11-11 15:53:07 -0300

    Adicionando config de homolg

--- config/database.php
+++ config/database.php
@@ -2,6 +2,14 @@
     'prod' => [
         'displayErrorDetails' => false,
         "db" => [
+            "dsn" => "pgsql:host=condado;port=5432;dbname=snh_dhab",
+            "username" => "sys.snh_dhab",
+            "password" => "#BDsyssnhdh@b#"
+        ],
+    ],
+    'prod-fab' => [
+        'displayErrorDetails' => false,
+        "db" => [
             "dsn" => "pgsql:host=192.168.10.113;port=5432;dbname=sinat",
             "username" => "postgres",
             "password" => "pg01"
