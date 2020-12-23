a2c9856fb327ee7a158598d11719e25b23847172
Paulo Victor <paulovictor.teles@gmail.com>
2020-12-23 10:12:16 -0300

    Alterando cofiguração de banco

--- config/database.php
+++ config/database.php
@@ -2,6 +2,14 @@
     'prod' => [
         'displayErrorDetails' => false,
         "db" => [
+            "dsn" => "pgsql:host=10.214.50.168;port=5432;dbname=mdr_snh_dhab",
+            "username" => "usr_websis",
+            "password" => "mdr@123"
+        ],
+    ],
+    'homol' => [
+        'displayErrorDetails' => false,
+        "db" => [
             "dsn" => "pgsql:host=172.17.18.224;port=5432;dbname=mdr_snh_dhab",
             "username" => "usr_snh_dhab",
             "password" => "mdr@123"
